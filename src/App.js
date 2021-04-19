65ff65fed2c7419210c7445c2fe4a7158b01d851
pmascaraque <ssm.pablo@gmail.com>
2021-04-19 06:38:32 +0100

    app.js search city input bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+const api = {
+  key: "60eafcec4afef1397e02df3bcc16f20c",
+  base: "https://api.openweathermap.org/data/2.5",
+};
+
 function App() {
   return (
-null
+    <div className="app">
+      <main>
+        <div id="search-box">
+          <input
+            type="text"
+            id="search-bar"
+            placeholder="Search a city"
+          ></input>
+        </div>
+      </main>
+    </div>
   );
 }
 
