fa8dc58192fd913b7dd6727f55e2d849b3dbae9b
pmascaraque <ssm.pablo@gmail.com>
2021-04-19 07:09:20 +0100

    Datebuilder and location box

--- src/App.js
+++ src/App.js
@@ -4,6 +4,40 @@ const api = {
 };
 
 function App() {
+ 
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+  
+    return `${day} ${date} ${month} ${year}`
+  }
+ 
   return (
     <div className="app">
       <main>
@@ -14,6 +48,13 @@ function App() {
             placeholder="Search a city"
           ></input>
         </div>
+
+        <div id="location-box">
+          <div id="location"></div>
+          <div id="date">{dateBuilder(new Date())}</div>
+        </div>
+
+        
       </main>
     </div>
   );
